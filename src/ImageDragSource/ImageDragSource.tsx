ea04da02924bc3bdeaecd6bc6d8686ea9b9f3209
moserle <moserle@student.ethz.ch>
2019-10-21 19:51:42 +0200

    update package versions

--- src/ImageDragSource/ImageDragSource.tsx
+++ src/ImageDragSource/ImageDragSource.tsx
@@ -15,7 +15,6 @@ type ImageDragSourceProps = {
  */
 export const ImageDragSource = (props: ImageDragSourceProps) => {
   const { children, selectedItems, onmousedown, item } = props;
-  console.log(selectedItems);
   const spec = {
     item: {
       selectedItems: selectedItems,
